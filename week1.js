1fcbbf3356e63744781c9e6aec2b8b9f0c18bc57
Iamneo <techadmin@iamneo.ai>
2023-09-03 17:28:04 +0000

    Backup Commit

--- week1.js
+++ week1.js
@@ -1,1 +1,16 @@
 let cells=[",",",",",",",","];
+let currentPlayer = 'X';
+let result = document.querySelector('.result');
+let btns = document.querySelectorAll('.btn');
+let reset = document.getElementById('reset');
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
